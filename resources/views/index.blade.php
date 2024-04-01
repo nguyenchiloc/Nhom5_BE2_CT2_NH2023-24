a557a3a4dd885c86fe7d398daf3600d72b53f5d8
nguyenchiloc <22211tt0255@mail.tdc.edu.vn>
2024-04-01 10:24:03 +0700

    ;;;;

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!-- {{-- View này sẽ kế thừa giao diện từ layout->master --}} -->
+<!-- {{-- View này sẽ kế thừa giao diện từ layout->masternn  --}} -->
 @extends('layouts.master')
 
 @section('title')
