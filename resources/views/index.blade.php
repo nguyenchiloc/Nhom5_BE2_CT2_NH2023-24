538673e334c5d1717db5f991136e6248bb757988
nguyenchiloc <22211tt0255@mail.tdc.edu.vn>
2024-04-01 10:35:36 +0700

    update test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!-- {{-- View này sẽ kế thừa giao diện từ layout->masternnhhh ,,--}} -->
+<!-- {{-- View này sẽ kế thừa giao diện từ layout->master--}} -->
 @extends('layouts.master')
 
 @section('title')
